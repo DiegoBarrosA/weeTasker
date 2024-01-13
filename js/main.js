bb6ad75aecb034a922f13f6da0da7a7fedea9530
DiegoBarrosA <diegobarrosaraya@outlook.com>
2024-01-13 17:16:31 -0300

    Second version

--- js/main.js
+++ js/main.js
@@ -1,93 +1,75 @@
-const myTasks = [];
-const myStatuses = [];
-const backToMenu = () => alert("Going back to the main menu");
-const createTasks = () => confirm("There are no tasks, do you want to create one?");
-const adjustFrom0 = (x) => x-1;
-let selectTask = "";
-let listA = "";
-let counter = 0;
-let promptText = "";
-let whatToDo = prompt("What do you want to do?\n1.- View my tasks\n2.- Add a new task\n3.- Update the status of a task");
-const statuses = [ "To Do","In Progress","Done"];
-mainMenu();
-function mainMenu(){
-while (whatToDo != ""){
-    switch (String(whatToDo)){
-        case "1":
-            if(myTasks.length != 0){
-                selectOption("Tasks:\n",myTasks,myStatuses);
-            }else{
-                createTasks();
-            }
-            break;
-        case "2":
-            addTasks();
-            break;
-        case "3":
-        modifyStatus();
-            break;
-        default:
-        alert("Please enter a parseInt between 0 and 3")
-            break;
-    }
-    whatToDo = prompt("What do you want to do?\n1.- View my tasks\n2.- Add a new task\n3.- Update the status of a task");
+let storedTasks = JSON.parse(localStorage.getItem("myTasks") || "[]");
+let myTasks = storedTasks;
+let statuses = ['Open', 'In Progress', 'Done'];
+
+let handyString = "";
+
+let filterTasksStatuses = (x) => myTasks.filter(obj => {
+  return obj.status == x;
+})
+
+
+for (let i = 0; i < statuses.length; i++) {
+  handyString += `<option>${statuses[i]}</option>`
 }
-}
-function addTasks(){
-    let wantsToContinue = true;
-    
-    while (wantsToContinue){
-        let newTask = prompt("What needs to be done?");
-        if(newTask == null || newTask.trim() == ""){
-            alert("Please provide a task")
-        }else{
-            myTasks.push(newTask);
-            myStatuses.push(statuses[0]);
-        }
-        confirmPrompt = confirm("Do you want to add another task?");
-        if(!confirmPrompt){
-            wantsToContinue = false;
-        };
-        console.log(myTasks.indexOf)
-    };
-}
-function selectOption(promptText,values){
-    if(values == myTasks)
-    for (x of values ){
-        counter+=1;
-        listA += String(counter) + ".- " + x + " - "+ myStatuses[adjustFrom0(counter)]  + "\n";
+
+
+document.getElementById("statuses").innerHTML = handyString;
+handyString = "";
+/* for (let j = 0; j < statuses.length; j++) {
+  for (let i = 0; i < filterTasksStatuses(statuses[j]).length; i++) {
+    handyString += `<tr><td><input name='${filterTasksStatuses(statuses[j])[i].id}' value='true' type='checkbox' id='taskId-${filterTasksStatuses(statuses[j])[i].id}'> ${filterTasksStatuses(statuses[j])[i].summary}</td></tr>`
   }
-  else{
-    for (x of values ){
-        counter+=1;
-        listA += String(counter) + ".- " + x + "\n";
+  document.getElementById(statuses[j]).innerHTML = handyString;
+  handyString = "";
+} */
+
+for (let j = 0; j < statuses.length; j++) {
+  for (let i = 0; i < filterTasksStatuses(statuses[j]).length; i++) {
+    handyString += `<tr><td>${filterTasksStatuses(statuses[j])[i].summary}  <select onchange='transitionate(this.options[this.selectedIndex].value,this.options[this.selectedIndex].id)' id='taskId-${filterTasksStatuses(statuses[j])[i].id}'></select></td></tr>`
   }
+
+  document.getElementById(statuses[j]).innerHTML = handyString;
+  handyString = "";
+}
+for (let j = 0; j < myTasks.length; j++) {
+for (s in statuses) {
+handyString+= `<option id='optionId-${j}-${statuses[s]}' value='${statuses[s]}'>${statuses[s]}</option>`;
+document.getElementById(`taskId-${j+1}`).innerHTML = handyString;
+}
+for (s in statuses){
+if( document.getElementById('optionId-'+j+'-'+statuses[s]).value === myTasks[j].status  ){
+  document.getElementById('optionId-'+j+'-'+statuses[s]).setAttribute('selected', true);
+}
+}
+handyString ='';
+}
+
+function transitionate(chosen,name){
+
+  
+let idTrantionate = Number(name.match(/\d+/g));
+
+myTasks[idTrantionate].status = chosen;
+
+  console.log(myTasks[idTrantionate]);
+  localStorage.setItem("myTasks", JSON.stringify(myTasks));
+
+}
+function createTask() {
+  const currentDateTime = new Date().toISOString();
+  let inputSummary = document.getElementById("summary").value;
+  let newTask = {
+    id: myTasks.length + 1,
+    summary: inputSummary,
+    status: "Open",
+    creationDateTime: currentDateTime,
+    updateDateTime: currentDateTime
   }
-  let selectionMade =  prompt(promptText + listA);
-  console.log(selectionMade + " was selected.")
-  listA ="";
-  counter = 0;
-  return selectionMade;
-}
-function modifyStatus(){
-if(myTasks.length === 0){
-    createTasks();
-        if(createTasks){
-            addTasks();
-            backToMenu();
-            return 0;
-        } else {
-            backToMenu();
-            return 0;
-        }
-    }
-    selectTask = selectOption("Which one do you want to modify?:\n",myTasks);
-    console.log(myTasks.length);
-if(parseInt(selectTask) <= parseInt(myTasks.length)){
-    myStatuses[adjustFrom0(parseInt(selectTask))] = statuses[adjustFrom0(parseInt(selectOption("Where to transtion?\n",statuses)))]   
-} else if (parseInt(selectTask) === 0) {
-    backToMenu();
-}else {
-    alert("Task " + selectTask +" does not exist, please select a task between 1 and " + String(myTasks.length ))
-}
-}
+  myTasks.push(newTask)
+  localStorage.setItem("myTasks", JSON.stringify(myTasks));
+};
+
+
+
+
